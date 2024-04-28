47f73348d156c0152b58a5df676ecef7130a8fe6
joshuajonghankim <joshuajonghankim@gmail.com>
2024-04-27 22:11:51 -0400

    delete

--- my-app/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/my-app",
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-  
-  };
-  
-  module.exports = nextConfig;
